d14810645e50ba23cf0ac80f84460da75f17a47b
U-NW-DEN-WS16\Nancy <shannonj498@gmail.com>
2017-10-10 15:58:59 -0600

    add files

---
+++ src/minesweeper.js
@@ -0,0 +1,43 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+//The code in your while loop has the potential to place bombs on top of already existing bombs.
+  }
+  return board;
+}
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join('  |  ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: '); 
+printBoard(playerBoard);
+console.log('Bomb Board: ')
+printBoard(bombBoard);
